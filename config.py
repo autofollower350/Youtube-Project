76c6270a8a13ae2bff14dd51a98f0e4d73e3b455
autofollower350 <followerauto350@gmail.com>
2025-02-18 22:16:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 # Add configuration like API token, download folder, etc.
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+BOT_TOKEN = '7949674880:AAF6FtDotEnZ5m1NEK0cE7YKNHK_tisd8Mk'
 DOWNLOAD_FOLDER = './downloads'
